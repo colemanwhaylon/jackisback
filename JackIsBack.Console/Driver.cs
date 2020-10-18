ec924e918d2f98453d0568add938fc2ab0edc9e2
Whaylon Coleman <it@anitconsultant.com>
2020-10-18 15:44:40 -0700

    Completely experimental changes I don't want to loose.  Ignore this commit.

--- JackIsBack.Console/Driver.cs
+++ JackIsBack.Console/Driver.cs
@@ -1,43 +1,188 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Actor.Internal;
+using JackIsBack;
 using JackIsBack.Console.Messages;
+using Tweetinvi;
+using Tweetinvi.Auth;
+using Tweetinvi.Events;
+using Tweetinvi.Streaming;
 
 namespace JackIsBack.Console
 {
-    public class Driver
+    public class Driver : IDisposable
     {
-        private static ActorSystem TwitterStreamingActorSystem;
-
-        static void Main(string[] args)
+        //private static ActorSystem TwitterStreamingActorSystem;
+        private readonly HttpClient _client = new HttpClient();
+        private TwitterInfo _twitterInfo = new TwitterInfo();
+        private ISampleStream _sampleStream;
+        public static async Task Main(string[] args)
         {
+            //var jsonFilePath = @"C:\Users\Owner\source\repos\colemanwhaylon\jackisback\JackIsBack.Console\bin\Debug\netcoreapp3.1\tweetsample.json";
             var driver = new Driver();
-            var jsonFilePath = @"C:\Users\Owner\source\repos\colemanwhaylon\jackisback\JackIsBack.Console\bin\Debug\netcoreapp3.1\tweetsample.json";
+            try
+            {
+                var twitterInfo = new TwitterInfo();
+                var userClient = new TwitterClient(twitterInfo.Secrets.Key, twitterInfo.Secrets.SecretKey,
+                    twitterInfo.Secrets.AccessToken, twitterInfo.Secrets.AccessTokenSecret);
 
-            TwitterStreamingActorSystem = ActorSystemImpl.Create("TwitterStreamingActorSystem");
-            System.Console.WriteLine("Actor system created");
+                var user = await userClient.Users.GetAuthenticatedUserAsync();
+                driver._sampleStream = userClient.Streams.CreateSampleStream();
+                driver._sampleStream.TweetReceived += sampleStreamOnTweetReceived();
 
-            Props streamRetrieverActorProps = Props.Create<StreamRetrieverActor>();
-            IActorRef streamingActorRef = TwitterStreamingActorSystem.ActorOf(streamRetrieverActorProps, "StreamRetrieverActor");
+                await driver._sampleStream.StartAsync();
+                System.Console.WriteLine(user);
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine(exception);
+            }
+            finally
+            {
+                driver._sampleStream.TweetReceived -= sampleStreamOnTweetReceived();
+                driver._sampleStream.TweetReceived -= null;
+            }
 
-            var jsonFile = driver.ReadJsonFile(jsonFilePath);
-            var tweet = driver.DeserializeTweet(jsonFile);
+            //TwitterStreamingActorSystem = ActorSystemImpl.Create("TwitterStreamingActorSystem");
+            //System.Console.WriteLine("Actor system created");
 
-            streamingActorRef.Tell(new TwitterMessage(tweet));
-            
+            //Props streamRetrieverActorProps = Props.Create<StreamRetrieverActor>();
+            //IActorRef streamingActorRef = TwitterStreamingActorSystem.ActorOf(streamRetrieverActorProps, "StreamRetrieverActor");
+
+            //ITweetRetriever tweetReciever = new FileTweetRetriever();
+            //var tweets = await tweetReciever.GetFileAsync(jsonFilePath);
+
+            //foreach (var root in tweets)
+            //{
+            //    streamingActorRef.Tell(new TwitterMessage(root));
+            //}
 
             //var jsonFile = driver.ReadJsonFile(jsonFilePath);
             //driver.WriteOutputToScreen(jsonFile);
-            TwitterStreamingActorSystem.Terminate();
+            //TwitterStreamingActorSystem.Terminate();
 
 
             System.Console.WriteLine("FINISHED!");
             System.Console.ReadLine();
         }
 
-        private string ReadJsonFile(string jsonFilePath)
+        private static EventHandler<TweetReceivedEventArgs> sampleStreamOnTweetReceived()
+        {
+            long count = 0;
+            return (sender, eventArgs) =>
+            {
+                System.Console.WriteLine($"count: {count++}\t" +  eventArgs.Tweet);
+            };
+        }
+
+        private async Task ProcessRepositories()
+        {
+            //_client.BaseAddress = new Uri(_twitterInfo.ApiLinks.Stream);
+            //_client.DefaultRequestHeaders.Accept.Clear();
+
+            //_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",  _twitterInfo.Secrets.BearerToken);
+
+            //var stringTask = await _client.GetStringAsync(_twitterInfo.ApiLinks.Stream);
+            //var streamTask = _client.GetStreamAsync(_twitterInfo.ApiLinks.Stream);
+
+            //await foreach (var x in stringTask)
+            //{
+            //}
+
+            //var msg = GetStreamData();
+
+            //await foreach (var s in msg)
+            //{
+            //    System.Console.WriteLine(s);
+            //}
+
+            //System.Console.WriteLine(msg);
+
+
+
+
+            System.Console.ReadLine();
+        }
+
+
+        //public void WriteOutputToScreen(string jsonString)
+        //{
+        //    try
+        //    {
+        //        var modelJson = DeserializeTweet(jsonString);
+
+        //        System.Console.WriteLine(modelJson);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Console.WriteLine(ex.Message);
+        //    }
+        //}
+        public async IAsyncEnumerable<string> GetStreamData()
+        {
+            _client.BaseAddress = new Uri(_twitterInfo.ApiLinks.Stream);
+            _client.DefaultRequestHeaders.Accept.Clear();
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _twitterInfo.Secrets.BearerToken);
+
+            var count = 0;
+            var r = new Random();
+            while (true)
+            {
+                System.Console.WriteLine(count++);
+                var streamString = await _client.GetStringAsync(_twitterInfo.ApiLinks.Stream).ConfigureAwait(false); 
+                System.Console.WriteLine(streamString);
+                yield break;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+    }
+
+
+    public interface ITweetRetriever
+    {
+        Task<List<Root>> GetTweetsAsync(string resource);
+        Task<string[]> GetFileAsync(string resource);
+    }
+
+    public class FileTweetRetriever : ITweetRetriever
+    {
+        public async Task<List<Root>> GetTweetsAsync(string resource)
+        {
+            var result = await Task.Factory.StartNew(() =>
+                                    {
+                                        var jsonFile = ReadJsonFile(resource);
+                                        var tweet = DeserializeTweet(jsonFile);
+                                        return tweet;
+                                    });
+            return result;
+        }
+
+        public async Task<string[]> GetFileAsync(string resource)
+        {
+            var result = await Task.Factory.StartNew(() =>
+            {
+                var jsonFile = ReadJsonFile(resource);
+                var file = DeserializeFile(jsonFile);
+                return file;
+            });
+            return result;
+        }
+
+        private static string ReadJsonFile(string jsonFilePath)
         {
             var options = new JsonSerializerOptions
             {
@@ -45,25 +190,21 @@ namespace JackIsBack.Console
                 WriteIndented = true
             };
 
-            var file = File.ReadAllText(jsonFilePath);
+            var file = File.ReadAllText(jsonFilePath, Encoding.Unicode);
+            var json = file.Split("}\r\n}{", StringSplitOptions.None);
+
+
+            //MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(file);
+            //MemoryMappedViewStream mms = mmf.CreateViewStream();
+            //using (BinaryReader b = new BinaryReader(mms))
+            //{
+            //}
+
+            //
             return file;
         }
 
-        private void WriteOutputToScreen(string jsonString)
-        {
-            try
-            {
-                var modelJson = DeserializeTweet(jsonString);
-
-                System.Console.WriteLine(modelJson);
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine(ex.Message);
-            }
-        }
-
-        private  Root DeserializeTweet(string jsonString)
+        private static List<Root> DeserializeTweet(string jsonString)
         {
             var options = new JsonSerializerOptions
             {
@@ -71,7 +212,20 @@ namespace JackIsBack.Console
                 WriteIndented = true
             };
 
-            var jsonModel = JsonSerializer.Deserialize<Root>(jsonString, options);
+            var jsonModel = JsonSerializer.Deserialize<List<Root>>(jsonString, options);
+
+            return jsonModel;
+        }
+
+        private static string[] DeserializeFile(string jsonString)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var jsonModel = JsonSerializer.Deserialize<string[]>(jsonString, options);
 
             return jsonModel;
         }
---
+++ JackIsBack.Console/TwitterInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace JackIsBack.Console
+{
+    public class TwitterInfo
+    {
+        public TwitterSecrets Secrets { get; set; }
+        public TwitterApiLinks ApiLinks { get; set; }
+
+        public TwitterInfo()
+        {
+            Secrets = new TwitterSecrets();
+            ApiLinks = new TwitterApiLinks();
+        }
+    }
+
+    public static class TwitterConfig
+    {
+        public static IConfigurationRoot GetConfigurationRoot()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configurationRoot = builder.Build();
+            return configurationRoot;
+        }
+    }
+
+    public class TwitterSecrets
+    {
+        public TwitterSecrets()
+        {
+            var icr = TwitterConfig.GetConfigurationRoot();
+            Key = icr.GetSection("Twitter:Secrets:Key").Value;
+            SecretKey = icr.GetSection("Twitter:Secrets:SecretKey").Value;
+            BearerToken = icr.GetSection("Twitter:Secrets:BearerToken").Value;
+            AccessToken = icr.GetSection("Twitter:Secrets:AccessToken").Value;
+            AccessTokenSecret = icr.GetSection("Twitter:Secrets:AccessTokenSecret").Value;
+        }
+
+        public string Key { get; set; }
+        public string SecretKey { get; set; }
+        public string BearerToken { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+    }
+
+    public class TwitterApiLinks
+    {
+        public TwitterApiLinks()
+        {
+            IConfigurationRoot icr = TwitterConfig.GetConfigurationRoot();
+
+            MultipleTweets = icr.GetSection("Twitter:ApiLinks:MultipleTweets").Value;
+            RecentSearch = icr.GetSection("Twitter:ApiLinks:RecentSearch").Value;
+            SingleTweet = icr.GetSection("Twitter:ApiLinks:SingleTweet").Value;
+            Stream = icr.GetSection("Twitter:ApiLinks:Stream").Value;
+            UserById = icr.GetSection("Twitter:ApiLinks:UserById").Value;
+            UserByUsername = icr.GetSection("Twitter:ApiLinks:UserByUsername").Value;
+            UsersById = icr.GetSection("Twitter:ApiLinks:UsersById").Value;
+            UsersByUsername = icr.GetSection("Twitter:ApiLinks:UsersByUsername").Value;
+        }
+        public string SingleTweet { get; set; }
+        public string MultipleTweets { get; set; }
+        public string UserById { get; set; }
+        public string UsersById { get; set; }
+        public string UserByUsername { get; set; }
+        public string UsersByUsername { get; set; }
+        public string RecentSearch { get; set; }
+        public string Stream { get; set; }
+    }
+}
